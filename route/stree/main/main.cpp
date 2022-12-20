73010cfa64ffe0b22647a9a259be8e2756e7c5aa
林殿倫 <luan@hayashidonorins-MacBook-Pro.local>
2022-12-20 15:57:49 +0800

    update

--- route/stree/main/main.cpp
+++ route/stree/main/main.cpp
@@ -1,12 +1,14 @@
 
 #include <src/stree.hpp>
+#include <fstream>
 
 int main(int argc, char** argv) {
-  if(argc != 2) {
+  if(argc != 3) {
     throw std::runtime_error("Number of parameters should be exact 2!\n");
   }
   std::filesystem::path input_file{argv[1]};
+  std::ofstream output_file{argv[2]};
   st::Stree st(input_file);
   st.apply();
-  st.dump(std::cerr);
+  st.dump(output_file);
 }
